6fefa404dae442422a9d0fd91f92faa331ac94b9
mesropt <mesroptnamak@gmail.com>
2022-05-19 10:51:55 +0300

    lesson_2 Finished tasks 1-4

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,23 +2,26 @@ import React from 'react';
 import axios from 'axios';
 import logo from './logo.svg'; // Лого мы удалили, можно без этого импорта.
 import './App.css'; // Подгрузка стилей.
-import AuthorList from './components/Author.js'
+import AuthorList from './components/Author.js';
+import UserList from './components/User.js';
+import FooterItem from './components/Footer.js';
+import MenuItem from './components/Menu.js';
 
 class App extends React.Component { // Создаём класс App, который наследуется от React.Component. Класс App в нашей One Way Data Flow является верхним и самым главным, имеет состояние. Все остальные компоненты будут простыми функциями в рамках нашей реализации.
   constructor(props) { // В конструктор класс передаём объект props (constructor в JS как init в Python).
     super(props) // Вызываем объект props.
     this.state = { // Объявляем состояние (this в JS как self в Python). То есть глобальное состояние есть только у класс App. Оно объявлено через этот this.
-      'authors': [] // В authors мы будем хранить массив наших авторов, которые мы будем получать с бекенда.
+      'users': [] // В users мы будем хранить массив наших авторов, которые мы будем получать с бекенда.
     }
   }
 
   componentDidMount() {
-    axios.get('http://127.0.0.1:8000/api/authors') // get-запрос к урлу, который нас интересует.
+    axios.get('http://127.0.0.1:8000/api/users') // get-запрос к урлу, который нас интересует.
       .then(response => { // Получаем response, через который забираем data (данные).
-        const authors = response.data
+        const users = response.data
           this.setState( // Изменяем state.
           {
-            'authors': authors
+            'users': users
           }
         )
       }).catch(error => console.log(error)) // Используя catch ловим ошибки и выводим их в log.
@@ -27,7 +30,9 @@ class App extends React.Component { // Создаём класс App, котор
   render () { // Данный метод отвечает за отрисовку нашего компонента (то есть так же, как в Django был такой же метод, который рендерил наши templates).
     return ( // Нижу внутри div можно писать по факту что угодно. Здесь используются div, то есть это сама html, которая будет рендериться.
         <div>
-          <AuthorList authors={this.state.authors} />
+          <MenuItem menu={'Menu'} />
+          <UserList users={this.state.users} />
+          <FooterItem footer={'Footer'} />
         </div>
     )
   }
--- readme.md
+++ readme.md
@@ -1,1 +1,16 @@
-Учебный проект
+# Учебный проект на основе Django Rest Framework
+
+Учебный проект, который разработан при прохождении обучения на факультете 
+"[Python-разработка](https://gb.ru/geek_university/python)", портал GeekBrains, на курсе `Django Framework`.
+
+## Стек
+
+* `Python` > 3.8.8
+* `Django`
+* `PyCharm`
+* `SQLite3`
+
+## Лицензия
+
+MIT
+
